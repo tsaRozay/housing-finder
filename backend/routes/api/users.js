785e6e0b6d1c1ac3592e2efc6ea77555cb5d073f
Your Name <you@example.com>
2024-10-09 23:04:03 -0400

    Add User signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bcrypt = require('bcryptjs')
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const router = express.Router();
 const { User } = require("../../db/models");
 
@@ -27,16 +29,29 @@ router.get("/current", async (req, res) => {
     }
 });
 
-// Create a new user (Sign Up)
-router.post("/signup", async (req, res) => {
-    try {
-        const { email, password, username } = req.body;
-        const newUser = await User.create({ email, password, username });
-        res.status(201).json(newUser);
-    } catch (error) {
-        res.status(500).json({ message: "Error creating user" });
+
+
+// Sign up
+router.post(
+    '/',
+    async (req, res) => {
+      const { email, password, username } = req.body;
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({ email, username, hashedPassword });
+  
+      const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      };
+  
+      await setTokenCookie(res, safeUser);
+  
+      return res.json({
+        user: safeUser
+      });
     }
-});
+  );
 
 // Get a specific user by ID
 router.get("/:id", async (req, res) => {
