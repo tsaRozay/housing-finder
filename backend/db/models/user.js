ef2ff1833f7f0b4d787be13bdb524c5f78a45eac
Your Name <you@example.com>
2024-10-11 13:25:50 -0400

    left off phase 5 adding attributes line 462

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -11,6 +11,20 @@ module.exports = (sequelize, DataTypes) => {
 
   User.init(
     {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 50],
+        },
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 50],
+        },
+      },
       username: {
         type: DataTypes.STRING,
         allowNull: false,
