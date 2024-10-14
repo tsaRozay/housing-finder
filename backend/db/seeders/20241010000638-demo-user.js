176e01221deeebe1ca3c65b15dcaf5860409cf5e
tsaRozay <josegarciaemail96@gmail.com>
2024-10-14 19:14:39 -0400

    fixing schema files2

--- backend/db/seeders/20241010000638-demo-user.js
+++ backend/db/seeders/20241010000638-demo-user.js
@@ -13,25 +13,28 @@ module.exports = {
     }
     options.tableName = 'Users';
 
-    await User.bulkCreate([
-      {
-        email: 'demo@user.io',
-        username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password', 10),
-      },
-      {
-        email: 'user1@user.io',
-        username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2', 10),
-      },
-      {
-        email: 'user2@user.io',
-        username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3', 10),
-      }
-    ], { validate: true, ...options });
-
-    console.log('Users successfully seeded');
+    try {
+      await User.bulkCreate([
+        {
+          email: 'demo@user.io',
+          username: 'Demo-lition',
+          hashedPassword: bcrypt.hashSync('password', 10),
+        },
+        {
+          email: 'user1@user.io',
+          username: 'FakeUser1',
+          hashedPassword: bcrypt.hashSync('password2', 10),
+        },
+        {
+          email: 'user2@user.io',
+          username: 'FakeUser2',
+          hashedPassword: bcrypt.hashSync('password3', 10),
+        }
+      ], { validate: true, ...options });
+      console.log('Users successfully seeded');
+    } catch (error) {
+      console.error('Error seeding users:', error);
+    }
   },
 
   async down(queryInterface, Sequelize) {
@@ -44,8 +47,13 @@ module.exports = {
     options.tableName = 'Users';
 
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
-      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
-    }, {});
+    try {
+      await queryInterface.bulkDelete(options, {
+        username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+      }, {});
+      console.log('Users successfully removed');
+    } catch (error) {
+      console.error('Error removing seeded users:', error);
+    }
   }
 };
