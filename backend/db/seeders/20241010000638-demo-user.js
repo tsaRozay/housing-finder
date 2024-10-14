1034c2c7bdad24b489a91662b49b49f4784ee971
tsaRozay <josegarciaemail96@gmail.com>
2024-10-14 19:07:03 -0400

    fixing schema files

--- backend/db/seeders/20241010000638-demo-user.js
+++ backend/db/seeders/20241010000638-demo-user.js
@@ -3,34 +3,46 @@
 const { User } = require('../models');
 const bcrypt = require("bcryptjs");
 
-let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
-}
-
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
+    console.log('Seeding Users...');
+
+    let options = {};
+    if (process.env.NODE_ENV === 'production' && process.env.SCHEMA) {
+      options.schema = process.env.SCHEMA;
+    }
+    options.tableName = 'Users';
+
     await User.bulkCreate([
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password')
+        hashedPassword: bcrypt.hashSync('password', 10),
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2')
+        hashedPassword: bcrypt.hashSync('password2', 10),
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3')
+        hashedPassword: bcrypt.hashSync('password3', 10),
       }
-    ], { validate: true });
+    ], { validate: true, ...options });
+
+    console.log('Users successfully seeded');
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
+    console.log('Removing seeded Users...');
+
+    let options = {};
+    if (process.env.NODE_ENV === 'production' && process.env.SCHEMA) {
+      options.schema = process.env.SCHEMA;
+    }
     options.tableName = 'Users';
+
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
       username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
