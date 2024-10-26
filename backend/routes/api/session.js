28bc97687a51f815f8218413b619a8b511bfbc88
tsaRozay <josegarciaemail96@gmail.com>
2024-10-26 14:03:51 -0400

    fixing session.js to match API docs

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -68,9 +68,9 @@ router.post("/", validateLogin, async (req, res, next) => {
             !user ||
             !bcrypt.compareSync(password, user.hashedPassword.toString())
         ) {
-            const err = new Error("Login failed");
+            const err = new Error("Invalid credentials");
             err.status = 401;
-            err.title = "Login failed";
+            err.title = "Invalid credentials";
             err.errors = {
                 credential: "The provided credentials were invalid.",
             };
