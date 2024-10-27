d19a3cf8c05332d10276ce49e5b39277b80991a4
Your Name <you@example.com>
2024-10-27 16:05:42 -0400

    fixing users.js route

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,10 +1,21 @@
 const { validationResult } = require('express-validator');
 
+// middleware for formatting errors from express-validator middleware
+// (to customize, see express-validator's documentation)
+const handleValidationErrors = (req, _res, next) => {
+  const validationErrors = validationResult(req);
 
-const handleValidationErrors = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-      return res.status(400).json({ message: "Bad Request", errors: errors.array() });
+  if (!validationErrors.isEmpty()) { 
+    const errors = {};
+    validationErrors
+      .array()
+      .forEach(error => errors[error.path] = error.msg);
+      
+    const err = Error("Bad Request");
+    err.errors = errors;
+    err.status = 400;
+    err.title = "Bad Request";
+    next(err);
   }
   next();
 };
