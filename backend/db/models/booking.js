afafbcf91cb3d20d3479dab461ec65b6b6ce0447
tsaRozay <josegarciaemail96@gmail.com>
2024-10-28 19:21:28 -0400

    fixing models

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -1,53 +1,30 @@
-"use strict";
-const { Model } = require("sequelize");
+'use strict';
+
+const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-    class Booking extends Model {
-        static associate(models) {
-            Booking.belongsTo(models.Spot, { foreignKey: "spotId" });
-            Booking.belongsTo(models.User, { foreignKey: "userId" });
-        }
+  class Booking extends Model {
+    static associate(models) {
+        Booking.belongsTo(models.User, {
+          foreignKey: 'userId',
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE'
+        });
+      Booking.belongsTo(models.Spot, {
+        foreignKey: 'spotId',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      });
     }
-    Booking.init(
-        {
-            id: {
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-                allowNull: false,
-            },
-            spotId: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-            },
-            userId: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-            },
-            startDate: {
-                type: DataTypes.DATE,
-                allowNull: false,
-            },
-            endDate: {
-                type: DataTypes.DATE,
-                allowNull: false,
-            },
-            createdAt: {
-                type: DataTypes.DATE,
-                allowNull: false,
-                defaultValue: DataTypes.NOW,
-            },
-            updatedAt: {
-                type: DataTypes.DATE,
-                allowNull: false,
-                defaultValue: DataTypes.NOW,
-            },
-        },
-        {
-            sequelize,
-            modelName: "Booking",
-            tableName: "Bookings",
-        }
-    );
-    return Booking;
+  }
+  Booking.init({
+    spotId: { type: DataTypes.INTEGER, allowNull: false },
+    userId: { type: DataTypes.INTEGER, allowNull: false },
+    startDate: { type: DataTypes.DATE, allowNull: false },
+    endDate: { type: DataTypes.DATE, allowNull: false }
+  }, {
+    sequelize,
+    modelName: 'Booking'
+  });
+  return Booking;
 };
