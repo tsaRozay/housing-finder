97652c1fc18bfcfa4aefa8c0042c5a87c205e3b9
tsaRozay <josegarciaemail96@gmail.com>
2024-10-28 19:24:11 -0400

    fixing models

--- backend/db/models/reviews.js
+++ backend/db/models/reviews.js
@@ -1,72 +1,51 @@
-"use strict";
+'use strict';
 
-const { Model } = require("sequelize");
+const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-    class Review extends Model {
-        static associate(models) {
-            Review.belongsTo(models.User, {
-                foreignKey: "userId",
-                onDelete: "CASCADE",
-            });
-            Review.belongsTo(models.Spot, {
-                foreignKey: "spotId",
-                onDelete: "CASCADE",
-            });
-            Review.hasMany(models.ReviewImage, {
-                foreignKey: "reviewId",
-                onDelete: "CASCADE",
-            });
-        }
+  class Review extends Model {
+    static associate(models) {
+      Review.belongsTo(models.Spot, {
+        foreignKey: "spotId",
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      });
+      Review.belongsTo(models.User, {
+        foreignKey: 'userId',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      });
+      Review.hasMany(models.ReviewImage, {
+        foreignKey: 'reviewId',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      });
     }
+  }
+  
+  Review.init({
+    spotId: { type: DataTypes.INTEGER, allowNull: false },
+    userId: { type: DataTypes.INTEGER, allowNull: false },
+    review: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Review text is required" },
+        notEmpty: { msg: "Review text is required" }
+      }
+    },
+    stars: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: { args: [0], msg: 'Stars must be an integer from 0 to 5' },
+        max: { args: [5], msg: 'Stars must be an integer from 0 to 5' }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Review'
+  });
 
-    Review.init(
-        {
-            userId: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                references: {
-                    model: "Users",
-                    key: "id",
-                },
-            },
-            spotId: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                references: {
-                    model: "Spots",
-                    key: "id",
-                },
-            },
-            review: {
-                type: DataTypes.TEXT,
-                allowNull: false,
-            },
-            stars: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                validate: {
-                    min: 1,
-                    max: 5,
-                },
-            },
-            createdAt: {
-                type: DataTypes.DATE,
-                allowNull: false,
-                defaultValue: DataTypes.NOW,
-            },
-            updatedAt: {
-                type: DataTypes.DATE,
-                allowNull: false,
-                defaultValue: DataTypes.NOW,
-            },
-        },
-        {
-            sequelize,
-            modelName: "Review",
-            timestamps: true,
-        }
-    );
-
-    return Review;
+  return Review;
 };
