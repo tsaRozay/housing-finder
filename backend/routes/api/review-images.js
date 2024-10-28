a0b052956135c0d050522f818fe733853a46c8e5
tsaRozay <josegarciaemail96@gmail.com>
2024-10-28 19:07:07 -0400

    fixing users.js routes

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -1,86 +1,25 @@
-const express = require("express");
+const express = require('express');
+const { ReviewImage, Review } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
 const router = express.Router();
-const { ReviewImage, Review } = require("../../db/models");
-const { requireAuth } = require("../../utils/auth");
 
-// Delete a Review Image
-router.delete("/:imageId", requireAuth, async (req, res) => {
-    const reviewImage = await ReviewImage.findByPk(req.params.imageId);
+router.delete("/:imageId", requireAuth, async (req, res, next) => {
+  const { id: userId } = req.user;
+  const { imageId } = req.params;
 
-    // Error response: Couldn't find a Review Image with the specified id
-    if (!reviewImage) {
-        return res
-            .status(404)
-            .json({ message: "Review Image couldn't be found" });
-    }
+  try {
+    const reviewImage = await ReviewImage.findOne({ where: { id: imageId } });
+    if (!reviewImage) return res.status(404).json({ message: "Review Image couldn't be found" });
 
-    // Check if the user owns the review to delete the image
     const review = await Review.findByPk(reviewImage.reviewId);
-
-    // Unauthorized if the user does not own the review
-    if (!review || review.userId !== req.user.id) {
-        return res.status(403).json({ message: "Unauthorized" });
-    }
+    if (review.userId !== userId) return res.status(403).json({ message: "Unauthorized" });
 
     await reviewImage.destroy();
-
     res.status(200).json({ message: "Successfully deleted" });
+
+  } catch (err) {
+    next(err);
+  }
 });
-
-// // Add an image to a review
-// router.post("/reviews/:reviewId/images", requireAuth, async (req, res) => {
-//     try {
-//         const { imageUrl } = req.body;
-//         const review = await Review.findByPk(req.params.reviewId);
-
-//         if (!review) {
-//             return res.status(404).json({ message: "Review not found" });
-//         }
-
-//         // To make sure the user owns the review the image is being added to
-//         if (review.userId !== req.user.id) {
-//             return res.status(403).json({ message: "Unauthorized" });
-//         }
-
-//         // Limits 10 images per review
-//         const imageCount = await ReviewImage.count({
-//             where: { reviewId: review.id },
-//         });
-
-//         if (imageCount >= 10) {
-//             return res.status(400).json({
-//                 message: "Cannot upload more than 10 images per review",
-//             });
-//         }
-
-//         const newImage = await ReviewImage.create({
-//             reviewId: review.id,
-//             imageUrl,
-//         });
-
-//         res.status(201).json(newImage);
-//     } catch (error) {
-//         res.status(500).json({ error: error.message });
-//     }
-// });
-
-// // Get all images for a review
-// router.get("/reviews/:reviewId/images", async (req, res) => {
-//     try {
-//         const reviewImages = await ReviewImage.findAll({
-//             where: { reviewId: req.params.reviewId },
-//         });
-
-//         if (!reviewImages) {
-//             return res
-//                 .status(404)
-//                 .json({ message: "No images found for this review" });
-//         }
-
-//         res.json(reviewImages);
-//     } catch (error) {
-//         res.status(500).json({ message: "Error retrieving review images" });
-//     }
-// });
 
 module.exports = router;
