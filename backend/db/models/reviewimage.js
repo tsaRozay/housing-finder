e6cea3770870569e956f5748f91ee7b733c19664
tsaRozay <josegarciaemail96@gmail.com>
2024-10-28 19:28:31 -0400

    fixing models

--- backend/db/models/reviewimage.js
+++ backend/db/models/reviewimage.js
@@ -1,48 +1,29 @@
 'use strict';
+
 const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-    class ReviewImage extends Model {
-        static associate(models) {
-            ReviewImage.belongsTo(models.Review, {
-                foreignKey: 'reviewId',
-                onDelete: 'CASCADE',
-            });
-        }
+  class ReviewImage extends Model {
+    static associate(models) {
+      ReviewImage.belongsTo(models.Review, {
+        foreignKey: "reviewId",
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      });
     }
+  }
 
-    ReviewImage.init(
-        {
-            reviewId: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                references: {
-                    model: 'Reviews',
-                    key: 'id',
-                },
-                onDelete: 'CASCADE',
-            },
-            url: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            createdAt: {
-                type: DataTypes.DATE,
-                defaultValue: DataTypes.NOW,
-                allowNull: false,
-            },
-            updatedAt: {
-                type: DataTypes.DATE,
-                defaultValue: DataTypes.NOW,
-                allowNull: false,
-            },
-        },
-        {
-            sequelize,
-            modelName: 'ReviewImage',
-            tableName: 'ReviewImages',
-        }
-    );
+  ReviewImage.init({
+    reviewId: { type: DataTypes.INTEGER, allowNull: false },
+    url: {
+      type: DataTypes.STRING(2048),
+      allowNull: false,
+      validate: { isUrl: true }
+    }
+  }, {
+    sequelize,
+    modelName: 'ReviewImage'
+  });
 
-    return ReviewImage;
+  return ReviewImage;
 };
