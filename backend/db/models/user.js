4855aa18a8719ef5662724102f530ddec431965e
Your Name <you@example.com>
2024-10-28 21:37:00 -0400

    models

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -4,8 +4,8 @@ const { Model, Validator } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
-      User.hasMany(models.Spot, { foreignKey: 'userId', onDelete: 'CASCADE' });
-      User.hasMany(models.Booking, { foreignKey: 'userId', onDelete: 'CASCADE' });
+    //   User.hasMany(models.Spot, { foreignKey: 'userId', onDelete: 'CASCADE' });
+    //   User.hasMany(models.Booking, { foreignKey: 'userId', onDelete: 'CASCADE' });
       User.hasMany(models.Review, { foreignKey: 'userId', onDelete: 'CASCADE' });
     }
   }
