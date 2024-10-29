126a6c491573bd097b678ae7dd3bedca67da84b0
tsaRozay <josegarciaemail96@gmail.com>
2024-10-28 20:00:43 -0400

    fixing api routes

--- backend/app.js
+++ backend/app.js
@@ -1,112 +1,74 @@
+// backend/app.js
 const express = require("express");
-const routes = require("./routes");
 require("express-async-errors");
 const morgan = require("morgan");
 const cors = require("cors");
 const csurf = require("csurf");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
+const routes = require("./routes");
 const { environment } = require("./config");
 const isProduction = environment === "production";
+const app = express();
 const { ValidationError } = require("sequelize");
 
-require("dotenv").config();
-
-const app = express();
-
-// Logging middleware
 app.use(morgan("dev"));
-
-// Middleware to parse cookies
 app.use(cookieParser());
-
-// Middleware to parse JSON bodies
 app.use(express.json());
 
 // Security Middleware
 if (!isProduction) {
-    // enable cors only in development
-    app.use(cors());
+  app.use(cors());
 }
 
-// helmet helps set a variety of headers to better secure your app
 app.use(
-    helmet.crossOriginResourcePolicy({
-        policy: "cross-origin",
-    })
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin",
+  })
 );
 
 // Set the _csrf token and create req.csrfToken method
 app.use(
-    csurf({
-        cookie: {
-            secure: isProduction,
-            sameSite: isProduction && "Lax",
-            httpOnly: true,
-        },
-    })
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
 );
 
-// Root Route
-
-app.get("/", (req, res) => {
-    res.send("Welcome to the Housing Finder API!");
-});
-
-// Connect all the routes
-app.use(routes);
-
-// Importing routes
-const bookingsRoute = require("./routes/api/bookings");
-const reviewsRoute = require("./routes/api/reviews");
-const spotImagesRoute = require("./routes/api/spot-images");
-const spotsRoute = require("./routes/api/spots");
-const usersRoute = require("./routes/api/users");
-const sessionRoute = require("./routes/api/session");
-const reviewImagesRoute = require("./routes/api/review-images");
-
-//Using routes
-app.use("/api/bookings", bookingsRoute);
-app.use("/api/reviews", reviewsRoute);
-app.use("/api/spot-images", spotImagesRoute);
-app.use("/api/spots", spotsRoute);
-app.use("/api/users", usersRoute);
-app.use("/api/session", sessionRoute);
-app.use("/api/review-images", reviewImagesRoute);
+app.use(routes); // Connect all the routes
 
 // Catch unhandled requests and forward to error handler.
 app.use((_req, _res, next) => {
-    const err = new Error("The requested resource couldn't be found.");
-    err.title = "Resource Not Found";
-    err.errors = { message: "The requested resource couldn't be found." };
-    err.status = 404;
-    next(err);
+  const err = new Error("The requested resource couldn't be found.");
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
 });
 
 // Process sequelize errors
 app.use((err, _req, _res, next) => {
-    // check if error is a Sequelize error:
-    if (err instanceof ValidationError) {
-        let errors = {};
-        for (let error of err.errors) {
-            errors[error.path] = error.message;
-        }
-        err.title = "Validation error";
-        err.errors = errors;
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
     }
-    next(err);
+    err.errors = errors;
+  }
+  next(err);
 });
 
 // Error formatter
 app.use((err, _req, res, _next) => {
-    res.status(err.status || 500);
-    console.error(err);
-    res.json({
-        title: err.title || "Server Error",
-        message: err.message,
-        errors: err.errors,
-        stack: isProduction ? null : err.stack,
-    });
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    message: err.message,
+    errors: err.errors,
+  });
 });
 
 module.exports = app;
