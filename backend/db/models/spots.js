085fdc9278dab522a5f2baa878938419c39361b7
Your Name <you@example.com>
2024-10-28 21:35:41 -0400

    models

--- backend/db/models/spots.js
+++ backend/db/models/spots.js
@@ -4,10 +4,10 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Spot extends Model {
     static associate(models) {
-      Spot.belongsTo(models.User, { foreignKey: 'ownerId' });  
-      Spot.hasMany(models.Review, { foreignKey: 'spotId' });
-      Spot.hasMany(models.Booking, { foreignKey: 'spotId' });
-      Spot.hasMany(models.SpotImage, { foreignKey: 'spotId' });
+    //   Spot.belongsTo(models.User, { foreignKey: 'ownerId' });  
+    //   Spot.hasMany(models.Review, { foreignKey: 'spotId' });
+    //   Spot.hasMany(models.Booking, { foreignKey: 'spotId' });
+    //   Spot.hasMany(models.SpotImage, { foreignKey: 'spotId' });
     }
   }
 
