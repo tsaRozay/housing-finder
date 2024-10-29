5bdef87f904b8e21a914f47238d0b578011889e0
tsaRozay <josegarciaemail96@gmail.com>
2024-10-28 20:01:39 -0400

    fixing api routes

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,18 +1,19 @@
-const jwt = require('jsonwebtoken');
-const { jwtConfig } = require('../config');
-const { User } = require('../db/models');
-
+// backend/utils/auth.js
+const jwt = require("jsonwebtoken");
+const { jwtConfig } = require("../config");
+const { User } = require("../db/models");
 const { secret, expiresIn } = jwtConfig;
 
 // Sends a JWT Cookie
 const setTokenCookie = (res, user) => {
-  // Create the token
+  // Create the token.
   const safeUser = {
     id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
     email: user.email,
     username: user.username,
   };
-
   const token = jwt.sign(
     { data: safeUser },
     secret,
@@ -22,17 +23,16 @@ const setTokenCookie = (res, user) => {
   const isProduction = process.env.NODE_ENV === "production";
 
   // Set the token cookie
-  res.cookie('token', token, {
+  res.cookie("token", token, {
     maxAge: expiresIn * 1000, // maxAge in milliseconds
     httpOnly: true,
     secure: isProduction,
-    sameSite: isProduction && "Lax"
+    sameSite: isProduction && "Lax",
   });
 
   return token;
 };
 
-// Restore user middleware
 const restoreUser = (req, res, next) => {
   // token parsed from cookies
   const { token } = req.cookies;
@@ -46,28 +46,27 @@ const restoreUser = (req, res, next) => {
     try {
       const { id } = jwtPayload.data;
       req.user = await User.findByPk(id, {
-        attributes: ['id', 'email', 'username', 'createdAt', 'updatedAt'],
+        attributes: {
+          include: ["email", "createdAt", "updatedAt"],
+        },
       });
     } catch (e) {
-      res.clearCookie('token');
+      res.clearCookie("token");
       return next();
     }
 
-    if (!req.user) res.clearCookie('token');
+    if (!req.user) res.clearCookie("token");
 
     return next();
   });
 };
 
-// If there is no current user, return an error
-const requireAuth = function (req, _res, next) {
-    if (req.user) return next();
-  
-    const err = new Error('Authentication required');
-    err.title = 'Authentication required';
-    err.errors = { message: 'Authentication required' };
-    err.status = 401;
-    return next(err);
-  }
+const requireAuth = function (req, res, next) {
+  if (req.user) return next();
 
-module.exports = { setTokenCookie, restoreUser, requireAuth};
+  const err = new Error("Authentication required");
+  err.status = 401;
+  return next(err);
+};
+
+module.exports = { setTokenCookie, restoreUser, requireAuth };
