22d4bd7b23779dff273dc1e8fd17a9c660352dcf
Your Name <josegarciaemail96@gmail.com>
2025-05-07 18:59:46 -0400

    react store setup debug

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,23 +1,22 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import { thunk } from 'redux-thunk';
 
 
-const rootReducer = combineReducers({
-  // reducers will go here
-});
-
-const configureStore = async (preloadedState) => {
-  let enhancer;
+const rootReducer = combineReducers({});
   
-  if (import.meta.env.MODE === 'production') {
-    enhancer = applyMiddleware(thunk);
+
+  let enhancer;
+  if (import.meta.env.MODE === "production") {
+      enhancer = applyMiddleware(thunk);
   } else {
-    const logger = (await import("redux-logger")).default;
-    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-    enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+      const logger = (await import("redux-logger")).default;
+      const composeEnhancers =
+          window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+      enhancer = composeEnhancers(applyMiddleware(thunk, logger));
   }
-
-  return createStore(rootReducer, preloadedState, enhancer);
-};
-
-export default configureStore;
+  
+  const configureStore = (preloadedState) => {
+      return createStore(rootReducer, preloadedState, enhancer);
+  };
+  
+  export default configureStore;
