56029e60dfd9cf06a2b587ee5088ca0f53d682ee
Your Name <josegarciaemail96@gmail.com>
2025-05-08 14:39:01 -0400

    worrking on spots

--- frontend/src/components/SignUpFormModal/SignupFormModal.jsx
+++ frontend/src/components/SignUpFormModal/SignupFormModal.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useModal } from '../../context/modal';
-import { Navigate } from 'react-router-dom';
 import * as sessionActions from '../../store/session';
 import './SignupForm.css';
 
