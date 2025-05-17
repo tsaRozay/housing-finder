32db5c78478461f78bdd2372fd96eebb33fa62d1
Your Name <josegarciaemail96@gmail.com>
2025-05-16 20:31:07 -0400

    Landing Page testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,15 @@ import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import * as sessionActions from './store/session';
-
+import LoginFormModal from './components/LoginFormModal';
+import SignupFormModal from './components/SignUpFormModal';
+import LandingPage from './components/LandingPage';
+import {fetchSpots} from './store/spots'
+import SpotDetails from './components/SpotDetails';
+import CreateSpot from './components/CreateSpot';
+import ManageSpots from './components/ManageSpots';
+import UpdateSpot from './components/UpdateSpot';
+import ManageReviews from './components/ManageReviews';
 
 function Layout() {
   const dispatch = useDispatch();
@@ -11,13 +19,14 @@ function Layout() {
 
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => {
+      dispatch(fetchSpots())
       setIsLoaded(true)
     });
   }, [dispatch]);
 
   return (
     <>
-    <Navigation isLoaded={isLoaded} />
+      <Navigation isLoaded={isLoaded} />
       {isLoaded && <Outlet />}
     </>
   );
@@ -29,8 +38,40 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <h1>Welcome!</h1>
+        element: <LandingPage />
       },
+      {
+        path: '/login',
+        element: <LoginFormModal />
+      },
+      {
+        path: '/signup',
+        element: <SignupFormModal />
+      },
+      {
+        path: "/api/spots",
+        element: <CreateSpot/>
+      },
+      {
+        path: "api/spots/current",
+        element: <ManageSpots />
+      },
+      {
+        path: '/spots/:spotId',
+        element: <SpotDetails />
+      },
+      {
+        path: "/api/spots/:spotId/edit",
+        element: <UpdateSpot/>
+      },
+      {
+        path: "/api/reviews/current",
+        element: <ManageReviews />
+      },
+      {
+        path: '*',
+        element: <h1>Page Not Found</h1>
+      }
     ]
   }
 ]);
