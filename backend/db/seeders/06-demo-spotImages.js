af6dc6fffd9ea143daf8a075664efadd8a1b3657
Your Name <josegarciaemail96@gmail.com>
2025-05-19 19:37:12 -0400

    troubleshooting images

--- backend/db/seeders/06-demo-spotImages.js
+++ backend/db/seeders/06-demo-spotImages.js
@@ -2,8 +2,10 @@
 
 let options = {};
 if (process.env.NODE_ENV === "production") {
-    options.schema = process.env.SCHEMA; // Define schema in options object if in production
+    options.schema = process.env.SCHEMA;
 }
+
+const now = new Date();
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -16,24 +18,30 @@ module.exports = {
                     spotId: 1,
                     url: "https://images.pexels.com/photos/15067166/pexels-photo-15067166/free-photo-of-taxi-on-a-city-street.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
                     preview: true,
+                    createdAt: now,
+                    updatedAt: now,
                 },
                 {
                     spotId: 1,
                     url: "https://images.pexels.com/photos/2224861/pexels-photo-2224861.png",
                     preview: false,
+                    createdAt: now,
+                    updatedAt: now,
                 },
                 {
                     spotId: 2,
-                    url: "https://examplepicture.com/image3.jpghttps://images.pexels.com/photos/2695679/pexels-photo-2695679.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+                    url: "https://images.pexels.com/photos/2695679/pexels-photo-2695679.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
                     preview: true,
+                    createdAt: now,
+                    updatedAt: now,
                 },
             ],
-            { validate: true } // Enable validation for data integrity
+            { validate: true }
         );
     },
 
     down: async (queryInterface, Sequelize) => {
         options.tableName = "SpotImages";
-        return queryInterface.bulkDelete(options, null, {}); // Use options to support schema if in production
+        return queryInterface.bulkDelete(options, null, {});
     },
 };
